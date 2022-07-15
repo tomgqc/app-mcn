2862f66ce9d3b55438960fc6d3994e247d3b0a45
tchouin <1837066@etudiant.cegepvicto.ca>
2022-07-15 15:41:47 -0400

    adding routes config

---
+++ Config..tsx
@@ -0,0 +1,9 @@
+import HomeScreen from "./screens/Home";
+
+export const DeviceKey:string = "";
+
+export const BaseUrl:string = "https://api.facetec.com/api/v3.1/biometrics";
+
+type Paths = "/status"|"/session-token"|"/liveness-3d"|"/liveness-2d";
+
+//export const DefaultPage = <div><HomeScreen><HomeScreen/></div>
