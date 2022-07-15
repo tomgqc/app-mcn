2abe26d3b20f043d57eed1d8c403467718767bd2
tchouin <1837066@etudiant.cegepvicto.ca>
2022-07-15 15:43:10 -0400

    update config file

--- Config..tsx
+++
@@ -1,9 +0,0 @@
-import HomeScreen from "./screens/Home";
-
-export const DeviceKey:string = "";
-
-export const BaseUrl:string = "https://api.facetec.com/api/v3.1/biometrics";
-
-type Paths = "/status"|"/session-token"|"/liveness-3d"|"/liveness-2d";
-
-//export const DefaultPage = <div><HomeScreen><HomeScreen/></div>
