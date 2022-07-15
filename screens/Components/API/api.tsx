4b9246d55fb543cd5883963c76a61ec95d032a8a
tchouin <1837066@etudiant.cegepvicto.ca>
2022-07-15 12:10:35 -0400

    ajout composant requete

---
+++ screens/Components/API/api.tsx
@@ -0,0 +1,32 @@
+type Method = "GET"|"POST"|"DELETE|UPDATE";
+
+export interface Result{
+    data:any;
+}
+
+export async function Request(path:string,method:Method,body?:any):Promise<Result>{
+    let data:Result = {data:"None"};
+    if(path.trim().length <= 0){
+        data = {data:"Le chemin n'est pas valide!"}
+    }
+    else{
+        try{
+            let header = new Headers();
+            header.append("Content-Type","application/json");
+            let init : any = {method:method}
+            init.headers = header;
+            init.body = JSON.stringify(body);
+            await fetch(path,init).then(res => {
+                return res.text();
+            }).then(text => {
+                data.data = JSON.parse(text);
+                console.log(data);
+            });
+        }
+        catch(e){
+            console.error(e);
+        }
+    }
+    
+    return data;
+}
