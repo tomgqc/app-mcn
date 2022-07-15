0b8fff45842767f93448507912f64270b2f30017
tchouin <1837066@etudiant.cegepvicto.ca>
2022-07-15 15:31:04 -0400

    ajout custom request

--- screens/Components/API/api.tsx
+++ screens/Components/API/api.tsx
@@ -30,3 +30,28 @@ export async function Request(path:string,method:Method,body?:any):Promise<Resul
     
     return data;
 }
+
+export async function CustomRequest(path:string,method:Method,header:Headers,body?:any):Promise<Result>{
+    let data:Result = {data:"None"};
+    if(path.trim().length <= 0){
+        data = {data:"Le chemin n'est pas valide!"}
+    }
+    else{
+        try{
+            let req : any = {method:method}
+            req.headers = header;
+            req.body = JSON.stringify(body);
+            await fetch(path,req).then(res => {
+                return res.text();
+            }).then(text => {
+                data.data = JSON.parse(text);
+                console.log(data);
+            });
+        }
+        catch(e){
+            console.error(e);
+        }
+    }
+    
+    return data;
+}
